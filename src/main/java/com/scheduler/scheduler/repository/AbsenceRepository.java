4a92e9ce038451c2392a03de8a70575cb487612d
greg00ry <grzegorz.trzaskoma21@gmail.com>
2026-04-21 17:56:23 +0200

    Fix AbsenceRepository argument and Fix User table name

--- src/main/java/com/scheduler/scheduler/repository/AbsenceRepository.java
+++ src/main/java/com/scheduler/scheduler/repository/AbsenceRepository.java
@@ -1,6 +1,7 @@
 package com.scheduler.scheduler.repository;
 
+import com.scheduler.scheduler.model.Absence;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AbsenceRepository extends JpaRepository<AbsenceRepository, Long> {
+public interface AbsenceRepository extends JpaRepository<Absence, Long> {
 }
