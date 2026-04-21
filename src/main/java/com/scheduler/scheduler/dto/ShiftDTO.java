d95b380d1c68793af41b8e16aa3f737a29a5fe81
greg00ry <grzegorz.trzaskoma21@gmail.com>
2026-04-21 17:48:14 +0200

    Added ShiftDTO

---
+++ src/main/java/com/scheduler/scheduler/dto/ShiftDTO.java
@@ -0,0 +1,10 @@
+package com.scheduler.scheduler.dto;
+
+import java.time.LocalDateTime;
+
+public class ShiftDTO {
+    private Long id;
+    private LocalDateTime date;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+}
