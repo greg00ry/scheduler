41d16497e825c01ff4a573351b865b26be47f261
greg00ry <grzegorz.trzaskoma21@gmail.com>
2026-04-21 17:43:59 +0200

    Added Validation to AbsenceDTO

--- src/main/java/com/scheduler/scheduler/dto/AbsenceDTO.java
+++ src/main/java/com/scheduler/scheduler/dto/AbsenceDTO.java
@@ -2,13 +2,17 @@ package com.scheduler.scheduler.dto;
 
 import com.scheduler.scheduler.model.Shift;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
 public class AbsenceDTO {
-    @NotBlank
+    @NotNull
     private Long id;
+    @NotNull
     private Shift shift;
+    @NotBlank
     private String reason;
+    @NotNull
     private LocalDateTime reportedAt;
 }
