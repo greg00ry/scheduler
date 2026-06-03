e07f9edc5eda902bd5e715a641fbcbf2c6c03c33
greg00ry <grzegorz.trzaskoma21@gmail.com>
2026-06-03 21:09:10 +0200

    Added ScheduleRoutingService

--- src/main/java/com/scheduler/scheduler/service/schedule/ScheduleRoutingService.java
+++ src/main/java/com/scheduler/scheduler/service/schedule/ScheduleRoutingService.java
@@ -1,11 +1,49 @@
 package com.scheduler.scheduler.service.schedule;
 
+import com.scheduler.scheduler.dto.schedule.ScheduleDTO;
+import com.scheduler.scheduler.model.Role;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
-@Service
-public class ScheduleRoutingService {
-    public void routeToScheduleService(Authentication authentication) {
+import java.util.List;
 
+@Service
+@RequiredArgsConstructor
+public class ScheduleRoutingService {
+
+    private final ScheduleQueryService scheduleQueryService;
+
+    private String getRole(Authentication authentication) {
+        String role = authentication.getAuthorities().stream()
+                .findFirst()
+                .map(GrantedAuthority::getAuthority)
+                .orElseThrow(() -> new RuntimeException("Role not found"));
+        return role;
     }
+
+    public ScheduleDTO routeToGetSchedule(Long id, Authentication authentication) {
+        String role = getRole(authentication);
+        return switch (role) {
+            case "ROLE_ADMIN" -> scheduleQueryService.getSchedule(id);
+            case "ROLE_MANAGER" -> scheduleQueryService.getScheduleForManagerAndEmployee(id);
+            case "ROLE_EMPLOYEE" -> scheduleQueryService.getScheduleForManagerAndEmployee(id);
+            case "ROLE_OWNER" -> scheduleQueryService.getScheduleForManagerAndEmployee(id);
+            default -> throw new RuntimeException("Unauthorized");
+        };
+    }
+
+    public List<ScheduleDTO> routeToGetAllSchedules(Authentication authentication) {
+        String role = getRole(authentication);
+        return switch (role) {
+            case "ROLE_ADMIN" -> scheduleQueryService.getAllSchedules();
+            case "ROLE_MANAGER" -> scheduleQueryService.getAllSchedulesForManagerAndEmployee();
+            case "ROLE_EMPLOYEE" -> scheduleQueryService.getAllSchedulesForManagerAndEmployee();
+            case "ROLE_OWNER" -> scheduleQueryService.getAllSchedulesForManagerAndEmployee();
+            default -> throw new RuntimeException("Unauthorized");
+        };
+    }
+
+
 }
